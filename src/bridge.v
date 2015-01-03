8ce0b4a19feae5796d712c05fd17ab34577d05be
kwilson <kwilsonpdx@gmail.com>
2015-01-03 15:20:17 -0800

    bridge sm verilog sketched in. Not compiled yet

--- src/bridge.v
+++ src/bridge.v
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 
 module bridge(
+		input RESET,
 		input GPS_I0,
 		input GPS_I1,
 		input GPS_Q0,
@@ -11,30 +12,24 @@ module bridge(
 		output MCU_SS,
 		output MCU_MOSI);
 
-wire GPS_CLK_4_092;
-wire go_newdata;
+wire datardy;
+
 wire gps_i0_sync;
 wire gps_i1_sync;
 wire gps_q0_sync;
 wire gps_q1_sync;
 
-assign MCU_SS   = ~go_newdata;
+wire GPS_CLK_4_092;
+
+assign MCU_SS   = ~datardy;
 assign MCU_SCK  = gps_i0_sync & gps_i1_sync & gps_q0_sync & gps_q1_sync;
 assign MCU_MOSI = 1'b1;
-
-// CLK_DIV4: Simple clock Divide by 4  CoolRunner-II
-//    Xilinx HDL Language Template, version 14.7
-CLK_DIV4 CLK_DIV4_inst (
-		.CLKDV(GPS_CLK_4_092),    // Divided clock output
-		.CLKIN(GPS_CLK_16_368)     // Clock input
-);
-// End of CLK_DIV4_inst instantiation
 
 // Instantiate edge detection here
 asynch_edge_detect asynch_edge_detect_inst(
 		.SYNC_CLK_IN(MCU_CLK_25_000),
 		.ASYNC_IN(GPS_CLK_4_092),
-		.DETECT_OUT(go_newdata)
+		.DETECT_OUT(datardy)
 );
 
 synchronizer synch_inst_i0 (
@@ -61,6 +56,15 @@ synchronizer synch_inst_q1 (
 	.synch_output(gps_q1_sync)
 );
 
+// CLK_DIV4: Simple clock Divide by 4  CoolRunner-II
+//    Xilinx HDL Language Template, version 14.7
+CLK_DIV4 CLK_DIV4_inst (
+		.CLKDV(GPS_CLK_4_092),    // Divided clock output
+		.CLKIN(GPS_CLK_16_368)     // Clock input
+);
+// End of CLK_DIV4_inst instantiation
+
+
 // Instantiate bridge state machine here
 bridge_sm bridge_sm_inst (
 .GPS_I0(gps_i0_sync),
@@ -68,6 +72,8 @@ bridge_sm bridge_sm_inst (
 .GPS_Q0(gps_q0_sync),
 .GPS_Q1(gps_q1_sync),
 .MCU_CLK_25_000(GPS_CLK_25_000),
+.RESET(RESET),
+.DATAREADY(datardy),
 .MCU_SCK(MCU_SCK),
 .MCU_SS(MCU_SS),
 .MCU_SS(MCU_MOSI)
--- src/bridge_sm.v
+++ src/bridge_sm.v
@@ -7,195 +7,155 @@ module bridge_sm(
     input GPS_Q0,
     input GPS_Q1,
     input MCU_CLK_25_000,
-	 input RESET,
+	input RESET,
+	input DATAREADY,
     output MCU_SCK,
     output MCU_SS,
     output MCU_MOSI
     );
 
+    parameter reset_st          = 4'b0000;
+    parameter start_st          = 4'b0001;
+    parameter i0_st             = 4'b0010;
+    parameter i0_clk_st         = 4'b0011;
+    parameter i1_st             = 4'b0100;
+    parameter i1_clk_st         = 4'b0101;
+    parameter q0_st             = 4'b0110;
+    parameter q0_clk_st         = 4'b0111;
+    parameter q1_st             = 4'b1000;
+    parameter q1_clk_st         = 4'b1001;
+    parameter wait_dataready_st = 4'b1010;
+    parameter ss_release_st     = 4'b1011;
+    parameter state13           = 4'b1100;
+    parameter state14           = 4'b1101;
+    parameter state15           = 4'b1110;
+    parameter state16           = 4'b1111;
+
+	wire reset_counter;
+	reg ctr_restart;
 	reg bitcount_en;
-   reg [7:0] bitcounter;
-   
-	// Ok...maybe we don't need bitcount_en...it's there for now.
-   always @(posedge MCU_CLK_25_000)
-      if (RESET)
+    reg [7:0] bitcounter;
+
+	assign reset_counter = ctr_restart | RESET;
+	
+    always @(posedge MCU_CLK_25_000)
+      if (reset_counter)
          bitcounter <= 0xFF;
       else if (bitcount_en)
          bitcounter <= bitcounter - 1;
-					
-   parameter start_st  = 4'b0000;
-   parameter <state2>  = 4'b0001;
-   parameter <state3>  = 4'b0010;
-   parameter <state4>  = 4'b0011;
-   parameter <state5>  = 4'b0100;
-   parameter <state6>  = 4'b0101;
-   parameter <state7>  = 4'b0110;
-   parameter <state8>  = 4'b0111;
-   parameter <state9>  = 4'b1000;
-   parameter <state10> = 4'b1001;
-   parameter <state11> = 4'b1010;
-   parameter <state12> = 4'b1011;
-   parameter <state13> = 4'b1100;
-   parameter <state14> = 4'b1101;
-   parameter <state15> = 4'b1110;
-   parameter <state16> = 4'b1111;
+   (* FSM_ENCODING="SEQUENTIAL", SAFE_IMPLEMENTATION="YES", SAFE_RECOVERY_STATE="reset_st" *) reg [3:0] state = reset_st;
 
-   (* FSM_ENCODING="SEQUENTIAL", SAFE_IMPLEMENTATION="YES", SAFE_RECOVERY_STATE="<recovery_state_value>" *) reg [3:0] state = <state1>;
-
-   always@(posedge <clock>)
-      if (<reset>) begin
-         state <= <state1>;
-         <outputs> <= <initial_values>;
+   always@(posedge MCU_CLK_25_000)
+      if (RESET) begin
+         state    <= reset_st;
+         MCU_SCK  <= 1'b1 ;
+         MCU_SS   <= 1'b1 ;
+         MCU_MOSI <= 1'b1 ;
       end
       else
          (* PARALLEL_CASE, FULL_CASE *) case (state)
-            start_st : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+            reset_st : begin
+				ctr_restart <= 1'b1;
+                MCU_SCK     <= 1'b1 ;
+                MCU_SS      <= 1'b1 ;
+                MCU_MOSI    <= 1'b1 ;
+                bitcount_en <= 1'b0;
+                state       <= start_st;
             end
-            <state2> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+			start_st : begin
+				if (DATAREADY) begin
+					MCU_SS  <= 1'b0;       // assert SS here
+					MCU_CLK <= 1'b1;       // Data will be clocked out of bridge on negative edge of MCU_CLK, into MCU on positive edge
+					state   <= i0_st;
+				end
+				else begin
+					state   <= start_st;
+				end
+				bitcount_en <= 1'b0;
+				ctr_restart <= 1'b0;
+			end
+            i0_st : begin
+               bitcount_en  <= 1'b1;
+               MCU_CLK      <= 1'b1;
+               state        <= i0_clk_st;
             end
-            <state3> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+            i0_clk_st: begin
+               bitcount_en  <= 1'b0;
+			   MCU_MOSI     <= GPS_I0;
+               MCU_CLK      <= 1'b0;
+               state        <= i1_st;
             end
-            <state4> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+            i1_st : begin
+               bitcount_en  <= 1'b1;
+               MCU_CLK      <= 1'b1;
+               state        <= i0_clk_st;
             end
-            <state5> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+            i1_clk_st : begin
+               bitcount_en  <= 1'b0;
+			   MCU_MOSI     <= GPS_I1;
+               MCU_CLK      <= 1'b0;
+               state        <= q0_st;
             end
-            <state6> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+            q0_st: begin
+               bitcount_en  <= 1'b1;
+               MCU_CLK      <= 1'b1;
+               state        <= q0_clk_st;
             end
-            <state7> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+            q0_clk_st : begin
+               bitcount_en  <= 1'b0;
+			   MCU_MOSI     <= GPS_Q0;
+               MCU_CLK      <= 1'b0;
+               state        <= q1_st;
             end
-            <state8> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+            q1_st : begin
+               bitcount_en  <= 1'b1;
+               MCU_CLK      <= 1'b1;
+               state        <= q1_clk_st;
             end
-            <state9> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+			q1_clk_st : begin
+				bitcount_en <= 1'b0;
+				MCU_MOSI    <= GPS_Q1;
+				MCU_CLK     <= 1'b0;
+				if(bitcounter == 0)  begin
+					state   <= ss_release_st;
+			    end 
+				else begin
+					state   <= wait_dataready_st;
+			    end
+			end
+            wait_dataready_st : begin
+			   if (DATAREADY) begin
+                  MCU_SS    <= 1'b0;       // assert SS here
+                  state     <= i0_st;
+		       end
+			   else begin
+                  MCU_SS    <= 1'b1;       // assert SS here
+                  state     <= wait_dataready_st;
+		       end
+               MCU_CLK     <= 1'b1;       
+               bitcount_en <= 1'b0;
             end
-            <state10> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
-            end
-            <state11> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
-            end
-            <state12> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+            ss_release_st : begin
+			   bitcount_en <= 1'b0;
+			   ctr_restart <= 1'b1;
+               MCU_CLK     <= 1'b1; 
+               MCU_SS      <= 1'b1;       // de-assert SS here
+               state       <= wait_dataready_st;
             end
             <state13> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+               state <= reset_st;
             end
             <state14> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+               state <= reset_st;
             end
             <state15> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+               state <= reset_st;
             end
             <state16> : begin
-               if (<condition>)
-                  state <= <next_state>;
-               else if (<condition>)
-                  state <= <next_state>;
-               else
-                  state <= <next_state>;
-               <outputs> <= <values>;
+               state <= reset_st;
             end
             default : begin  // Fault Recovery
-               state <= <state1>;
-               <outputs> <= <values>;
+               state <= reset_st;
             end   
          endcase
 							
